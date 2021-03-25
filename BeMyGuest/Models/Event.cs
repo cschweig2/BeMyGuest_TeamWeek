85f3ef21f442bf8051d5e35d3e210eb83b7ea18e
Bess Campbell <bess.k.campbell@gmail.com>
2021-03-25 15:20:52 -0700

    Fix date time formatting issue
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- BeMyGuest/Models/Event.cs
+++ BeMyGuest/Models/Event.cs
@@ -4,36 +4,36 @@ using System;
 
 namespace BeMyGuest.Models
 {
-    public class Event
+  public class Event
+  {
+    public Event()
     {
-        public Event()
-        {
-            this.JoinEntries = new HashSet<Gathering>();
-        }
-        public int EventId { get; set; }
-        public string EventTitle { get; set; }
-        [Display(Name="Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-dd-yyyy")]
-        public DateTime? EventDate { get; set; }
-        public string Venue { get; set; }
-        [Display(Name = "Venue Address")]
-        public string VenueAddress { get; set; }
-        [Display(Name = "Venue Requirements")]
-        public string VenueRequirements { get; set; }
-        [Display(Name = "Start Time")]
-        // [DataType(DataType.Time)]
-        // [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{HH:MM tt PST")]
-        public DateTime? StartTime { get; set; }
-        [Display(Name = "End Time")]
-        // [DataType(DataType.Time)]
-        // [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{HH:MM tt PST")]
-        public DateTime? EndTime { get; set; }
-        [Display(Name = "Quarantine Requirements")]
-        public string QuarantineRequirements { get; set; }
-        [Display(Name = "Mask Requirements")]
-        public bool MaskRequirements { get; set; }
-        public virtual ApplicationUser User { get; set; }
-        public virtual ICollection<Gathering> JoinEntries  { get; set; }
+      this.JoinEntries = new HashSet<Gathering>();
     }
+    public int EventId { get; set; }
+    public string EventTitle { get; set; }
+    [Display(Name = "Date")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-dd-yyyy}")]
+    public DateTime? EventDate { get; set; }
+    public string Venue { get; set; }
+    [Display(Name = "Venue Address")]
+    public string VenueAddress { get; set; }
+    [Display(Name = "Venue Requirements")]
+    public string VenueRequirements { get; set; }
+    [Display(Name = "Start Time")]
+    [DataType(DataType.Time)]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:hh:mm tt PST}")]
+    public DateTime? StartTime { get; set; }
+    [Display(Name = "End Time")]
+    [DataType(DataType.Time)]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:hh:mm tt PST}")]
+    public DateTime? EndTime { get; set; }
+    [Display(Name = "Quarantine Requirements")]
+    public string QuarantineRequirements { get; set; }
+    [Display(Name = "Mask Requirements")]
+    public bool MaskRequirements { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<Gathering> JoinEntries { get; set; }
+  }
 }
