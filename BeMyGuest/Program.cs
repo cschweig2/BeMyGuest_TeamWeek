ae84080e5b30e44246851f195af306415e942ae1
Bess Campbell <bess.k.campbell@gmail.com>
2021-03-25 15:52:58 -0700

    Add date/time formatting
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- BeMyGuest/Program.cs
+++ BeMyGuest/Program.cs
@@ -4,18 +4,18 @@ using Microsoft.AspNetCore;
 
 namespace BeMyGuest
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://localhost:5000/");
-        //API is hosted on 5001
-
+      CreateWebHostBuilder(args).Build().Run();
     }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseUrls("http://localhost:5000/");
+    //API is hosted on 5001
+
+  }
 }
